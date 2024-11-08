81e03c3eb7452b21fb8832b3b46e321e95f24891
hamdy-farg <farghamdy72@gmail.com>
2024-11-08 14:33:45 +0200

    .assets . images .svgs .navigator extension .approuter & routes added . thems color and styles added .empty login screen added . onboarding screen & get started widget and doctor image and name widget and doc_logo widget

---
+++ lib/features/onboarding/widgets/doc_logo_and_name.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:omar_ahmed_advanced_project/core/theming/styles.dart';
+
+class DocLogoAndName extends StatelessWidget {
+  const DocLogoAndName({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset('assets/svgs/docdoc_logo.svg'),
+        SizedBox(width: 10.w),
+        Text('DocDoc', style: TextStyles.font24Black700Weight)
+      ],
+    );
+  }
+}
